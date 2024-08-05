ffef06ffb83ea7f075287bc4e4489dff46ec8440
Flypans <sbkwon011@outlook.com>
2024-08-05 18:58:05 +0100

    Add test for Event and Delegate

---
+++ EventTest/EventAndDelegateCar.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace EventTest
+{
+    //Event Publisher 게시자
+    class EventAndDelegateCar
+    {
+        private int _fuelGauges;
+
+        public int FuleGauge
+        {
+            get { return _fuelGauges; }
+            set
+            {
+                _fuelGauges = value;
+                IlluminateLackOfFuel();
+            }
+        }
+        public EventAndDelegateCar()
+        {
+            FuleGauge = 25;
+        }
+        public void Go()
+        {
+            Console.WriteLine("Drive");
+            FuleGauge -= 5;
+        }
+
+        //1_Delegate and Event
+        /*
+                public delegate void FuelEmptyEventHandler();
+                public event FuelEmptyEventHandler LackOfFuel;
+        */
+
+        //2_Action<T>
+        public event Action LackOfFuel;
+
+        public void IlluminateLackOfFuel()
+        {
+            /*
+                        if(LackOfFuel != null)
+                        {
+                            LackOfFuel();
+                        }
+            */
+            Console.WriteLine($"Status: {FuleGauge} %");
+
+            if (FuleGauge <= 20)
+            {
+                LackOfFuel?.Invoke();// call if registered
+            }
+        }
+    }
+}
---
+++ EventTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTest
+{
+
+    //Event Subscriber
+    class EventAndDelegate
+    {
+        static void Main(string[] args)
+        {
+            EventAndDelegateCar car = new EventAndDelegateCar();
+            car.LackOfFuel += Car_LackOfFuel;
+            car.LackOfFuel += () =>
+            {
+                System.Console.ForegroundColor = System.ConsoleColor.Red;
+                System.Console.BackgroundColor = System.ConsoleColor.Blue;
+                System.Console.WriteLine("Fill the car with gas");
+                System.Console.ResetColor();
+            };
+
+            car.Go();
+            //car.IlluminateLackOfFuel();
+            car.Go();
+        }
+
+        //Event Handler
+        private static void Car_LackOfFuel()
+        {
+            System.Console.WriteLine(" Lack of Fuel !");
+        }
+    }
+}
