e0b47550ca8c129da26e41ad1776da820e951809
Flypans <sbkwon011@outlook.com>
2024-08-07 12:58:22 +0100

    Add test for Attribute and Reflection

---
+++ AttributeTest/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace AttributeTest
+{
+    public class LuxuryAttribute : Attribute 
+    {
+        public LuxuryAttribute()
+        {
+            Console.WriteLine("Kabiolett");
+        }
+    }
+    [Luxury]
+    //[LuxuryAttribute]
+
+    class Car
+    {
+        //[Obsolete]
+        [Obsolete("Manual will be removed from the next release version, so please use AutoGang.")]
+        public void Manual() => Console.WriteLine("Manual Gear ");
+        public void AutoGang() => Console.WriteLine("Auto Gear");
+
+        [Conditional("DEBUG")]
+        public void TestDrive() => Console.WriteLine("Test Drive");
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Manual();
+            car.AutoGang();
+            car.TestDrive(); // does not operate in Release mode.
+
+            //Attribute
+            Attribute.GetCustomAttributes(typeof(Car));
+            typeof(Car).GetCustomAttributes(false);
+
+            //Reflection
+            //Car carType = new Car();
+            var carType = (new Car());
+
+            Type myCar = carType.GetType();
+            MethodInfo info = myCar.GetMethod("AutoGang");
+            
+            info.Invoke(carType, null);
+
+        }
+    }
+}
