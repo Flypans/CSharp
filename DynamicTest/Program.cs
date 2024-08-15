a6a5f50ab1782f7b8a9a6ff8b73c330c9ab756c9
Flypans <sbkwon011@outlook.com>
2024-08-15 14:12:27 +0100

    Add test for dynamic type

---
+++ DynamicTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string staticString = "Hallo";
+            var staticVar = "World";
+            dynamic dynamicString = "12345";
+
+            Console.WriteLine($"{staticString.ToUpper()}:{staticString.GetType()}");
+            Console.WriteLine($"{staticVar.ToLower()}:{staticVar.GetType()}");
+            //Console.WriteLine($"{dynamicString.Length()}:{dynamicString.GetType()}");//Runtime Error
+            Console.WriteLine($"{dynamicString} : {dynamicString.GetType()}");
+        }
+    }
+}
