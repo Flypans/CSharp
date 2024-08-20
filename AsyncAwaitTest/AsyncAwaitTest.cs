4a53af4b3c15ae4da24b79c80a548d030040f399
Flypans <sbkwon011@outlook.com>
2024-08-20 12:46:46 +0100

    Add test for Async/Await

---
+++ AsyncAwaitTest/AsyncAwaitTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitTest
+{
+    class AsyncAwaitTest
+    {
+        //static void Main(string[] args)
+        static async Task Main(string[] args)
+        {
+            await DoAsync();
+        }
+        static async Task DoAsync()
+        {
+            using(var client = new HttpClient())
+            {
+                //await
+                var response =  await client.GetAsync("https://jsonplaceholder.typicode.com/posts");
+                var content = await response.Content.ReadAsStreamAsync();
+                //var content = await response.Content.ReadAsStringAsync();
+
+                Console.WriteLine(content);
+            }
+        }
+    }
+}
